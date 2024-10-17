d31c0cc83dbeebd3a6b97dd33684d604d1af4818
su-ri-ya <suriya6374570440@gmail.com>
2024-10-17 13:26:01 +0530

    dyanimc dtat sending

--- src/app.js
+++ src/app.js
@@ -1,36 +1,26 @@
-const express = require("express")
-const app = express()
-const connectDB =require("./config/database")
-const user = require("./model/user")
+const express = require("express");
+const app = express();
+const connectDB = require("./config/database");
+const User = require("./model/user");  // Assuming User is a Mongoose model
 
-app.post("/signup",async(req,res)=>{
-    const Userobj={
-        firstName:"suriya",
-        lastName:"riya",
-        emailId:"suriyabidb6@gmail.com",
-        password:"1234567",
-    }
-    const User =new user(Userobj)
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+    const newUser = new User(req.body);
+
     try {
-        await User.save()
-    res.send("user add sucussfully")
-        
+        await newUser.save();
+        res.status(201).send("User added successfully");
     } catch (error) {
-        res.status(400).send("error occurred")
-        
+        res.status(400).send("Error adding the user: " + error.message);
     }
-    
+});
 
-})
-
-
-connectDB().then(()=>{
-    console.log("db connnected sucussfully");
-    app.listen(6464,()=>{
-        console.log("server is running");
-        
-    })
-}).catch((err)=>{
-    console.log("db not connected");
-})
-
+connectDB().then(() => {
+    console.log("DB connected successfully");
+    app.listen(6464, () => {
+        console.log("Server is running on port 6464");
+    });
+}).catch((err) => {
+    console.log("DB connection failed: " + err.message);
+});
