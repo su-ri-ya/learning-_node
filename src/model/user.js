cd149f6ab9f9e15e481ea41feb9c611aa623ca63
su-ri-ya <suriya6374570440@gmail.com>
2024-10-17 20:17:59 +0530

    va;idatior,api and stuff

--- src/model/user.js
+++ src/model/user.js
@@ -1,24 +1,61 @@
+const validator=require("validator")
 const mongoose =require("mongoose")
 
 const UserSchema=mongoose.Schema({
     firstName:{
-        type:String
+        type:String,
+        required:true,
     },
     lastName:{
         type:String
     },
     emailId:{
-        type:String
+        type:String,
+        required:true,
+        unique:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("not correct email id")
+            }
+        }
     },
     password:{
-        type:String
+        type:String,
+        required:true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("not  strong enough...")
+            }
+        }
     },
     age:{
-        type:Number
+        type:Number,
+        min:18
     },
     gender:{
-        type:String
+        type:String,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw  new Error("gender not valid")
+            }
+        }
     },
-})
+    about:{
+        type:String,
+        default:"this is default about"
+    },
+    skills:{
+        type:[String]
+    },
+    photoUrl:{
+        type:String,
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("not correct Url")
+            }
+        }
+    }
+    
+},{timestamps:true})
 
 module.exports=mongoose.model("User",UserSchema)
